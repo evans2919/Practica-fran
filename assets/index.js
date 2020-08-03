51504ae4564d021cb6893b5eb694b42006020cc3
Evans <evansalcantara29@outlook.com>
2020-08-03 20:46:07 +0200

    cambios en los eventos del botón con JavaScript (cambio de color de fondos y letras)

---
+++ assets/index.js
@@ -0,0 +1,97 @@
+function iniciar() {
+    var button;
+    button = document.getElementById('button');
+    button.addEventListener('click', buttonClick);
+}
+
+function buttonClick() {
+    // Top BG Pattern
+
+    var topbg;
+    topbg = document.getElementById('dashboard');
+    topbg.style.backgroundColor = 'hsl(232, 19%, 15%)';
+
+    // Dashboard Tittle
+
+    var tittle;
+    tittle = document.getElementById('dashboard-tittle');
+    tittle.style.color = 'hsl(0, 0%, 100%)';
+
+    // BG
+
+    var bgDark;
+    bgDark = document.getElementById('bg-dark');
+    bgDark.style.backgroundColor = 'hsl(230, 17%, 14%)';
+
+    // Card-top BG
+
+    var cardBgFacebook;
+    cardBgFacebook = document.getElementById('card-bg-facebook');
+    cardBgFacebook.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBgFacebook.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBgTwitter;
+    cardBgTwitter = document.getElementById('card-bg-twitter');
+    cardBgTwitter.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBgTwitter.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBgInstagram;
+    cardBgInstagram = document.getElementById('card-bg-instagram');
+    cardBgInstagram.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBgInstagram.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBgYoutube;
+    cardBgYoutube = document.getElementById('card-bg-youtube');
+    cardBgYoutube.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBgYoutube.style.color = 'hsl(0, 0%, 100%)';
+
+    // Card-top hover
+
+    // Bottom tittle
+
+    var bottomTittle = document.getElementById('footer-tittle');
+    bottomTittle.style.color = 'hsl(0, 0%, 100%)';
+
+    // Card-bottom BG
+
+    var cardBottom1;
+    cardBottom1 = document.getElementById('card-bottom-1');
+    cardBottom1.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom1.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBottom2;
+    cardBottom2 = document.getElementById('card-bottom-2');
+    cardBottom2.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom2.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBottom3;
+    cardBottom3 = document.getElementById('card-bottom-3');
+    cardBottom3.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom3.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBottom4;
+    cardBottom4 = document.getElementById('card-bottom-4');
+    cardBottom4.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom4.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBottom5;
+    cardBottom5 = document.getElementById('card-bottom-5');
+    cardBottom5.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom5.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom5.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBottom6;
+    cardBottom6 = document.getElementById('card-bottom-6');
+    cardBottom6.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom6.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBottom7;
+    cardBottom7 = document.getElementById('card-bottom-7');
+    cardBottom7.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom7.style.color = 'hsl(0, 0%, 100%)';
+
+    var cardBottom8;
+    cardBottom8 = document.getElementById('card-bottom-8');
+    cardBottom8.style.backgroundColor = 'hsl(228, 28%, 20%)';
+    cardBottom8.style.color = 'hsl(0, 0%, 100%)';
+}
