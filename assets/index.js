6f4f77f85ea5dab24d40ae026b3b445eb1435ce3
Evans <evansalcantara29@outlook.com>
2020-08-04 15:32:23 +0200

    Optimización del cambio de hoja de estilos dinámica con JavaScript

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,14 @@
-function cambiarArchivoCss(archivo) {
-    document.getElementById('white-sheet').href = archivo;
+// Esta función de JS hace que cambie la hoja de estilos dinámicamente en el HTML dándole al botón switch (parte superior derecha)
+
+function changeStyleSheet() {
+	var changeBG = document.getElementById('change-bg');
+
+	if (changeBG.checked) {
+		document.getElementById('white-sheet').href = './assets/styles.css';
+		changeBG.checked = false;
+	} else {
+		document.getElementById('white-sheet').href = './assets/black-theme.css';
+		changeBG.checked = true;
+	}
+	console.log(changeBG);
 }
